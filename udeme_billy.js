80c85973911d1dd3578a1d4dbbd0d6763e5c07a5
Udeme <udemebilly@gmail.com>
2024-02-23 14:27:12 +0100

    First Commit

---
+++ udeme_billy.js
@@ -0,0 +1,10 @@
+let firstname = "Udeme";
+let lastname = "Billy";
+let myage = "2024";
+let mysalary = "10000";
+
+console.log(firstname + " " + lastname);
+
+console.log(
+  "I was born in the year" + " "  + myage + " and my salary expectetion is"+ " "  + mysalary
+);
