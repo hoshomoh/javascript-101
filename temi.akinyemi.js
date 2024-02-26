192ad6e1cd835b20e11645f7be5bb2b83c6f9ba7
LoluT <157151145+LoluT@users.noreply.github.com>
2024-02-26 13:42:10 -0500

    Update temi.akinyemi.js (correction)

--- temi.akinyemi.js
+++ temi.akinyemi.js
@@ -1,5 +1,5 @@
 const firstName = "Temi";
 const lastName = "Akinyemi";
-const fullName = ´${firstName} {lastName}´;
+const fullName = firstName + " " + lastName;
 const age = 21;
 const salaryExpectation = 100000.00;
