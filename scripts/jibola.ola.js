7467c2659395359a8e676a82e08f7ed2c43f0637
jibola.olayinka <opeaceajibola@yahoo.com>
2024-02-27 19:31:32 +0100

    Syntax error fixed

--- scripts/jibola.ola.js
+++ scripts/jibola.ola.js
@@ -1,5 +1,8 @@
 let firstName = "jibola";
 let lastName = "ola";
-let fullName = `${firstName} ${lastName}`;
+function getFullName (firstName, lastName) {
+    return `${firstName} ${lastName}`;
+}
+let fullName = getFullName (firstName, lastName);
 let age = 22;
 let salaryExpectation = 55000.00;
