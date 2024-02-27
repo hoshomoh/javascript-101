ccd7d72bbb3df353d2d8ac2d02b6782d54a4b08f
jibola.olayinka <opeaceajibola@yahoo.com>
2024-02-27 14:38:43 +0100

    changes made

--- scripts/jibola.ola.js
+++ scripts/jibola.ola.js
@@ -2,4 +2,4 @@ const firstName = "jibola";
 const lastName = "ola";
 const fullName = `${firstName} ${lastName}`;
 const age = 22;
-const salaryExpectationn = 65000.00;
+const salaryExpectationn = 55000.00;
